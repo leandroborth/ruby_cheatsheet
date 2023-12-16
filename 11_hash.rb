1b2961b0d3391d444b5cd10d4d89e174ed826136
Leandro Borth <116202867+leandroborth@users.noreply.github.com>
2023-12-15 23:51:43 -0300

    Add one more example of hash

--- 11_hash.rb
+++ 11_hash.rb
@@ -11,3 +11,11 @@ product.each do |key, value|
   puts key
   puts value
 end
+
+developers = {
+  1 => { name: "Dev 1", age: "30", country: "USA" },
+  2 => { name: "Dev 2", age: "40", country: "Canada" },
+  3 => { name: "Dev 3", age: "50", country: "Mexico" },
+  4 => { name: "Dev 4", age: "60", country: "Brazil" },
+  5 => { name: "Dev 5", age: "70", country: "Argentina" }
+}
